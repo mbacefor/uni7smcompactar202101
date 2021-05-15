a00b4449375e83fb8958844ec16cac9e7153370f
Marcelo <marcelo.alcantara@serpro.gov.br>
2021-05-15 18:27:45 -0300

    test: ajuste da classe de test

--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,15 @@
 'use strict';
 
 import { expect } from 'chai';
-import { Example } from '../dist/index';
+import { AlgoritmosCompressao } from '../dist/index';
 
-describe('Example class', () => {
+describe('AlgoritmosCompressao class', () => {
 	it('should create an instance using its constructor', () => {
-		const example: Example = new Example();
+		const example: AlgoritmosCompressao = new AlgoritmosCompressao();
 		expect(example, 'example should exist').to.exist; // tslint:disable-line:no-unused-expression
 	});
 	it('should return whatever is passed to exampleMethod()', () => {
-		const example: Example = new Example();
+		const example: AlgoritmosCompressao = new AlgoritmosCompressao();
 		const param: string = 'This is my param.';
 		const returnValue: string = example.exampleMethod(param);
 		expect(returnValue).to.equal(param, 'returns the value passed as a parameter');
